dcca28fdf491fd1e13849ee8c1010bf211a37fe4
Yuvaprakash24 <114161768+Yuvaprakash24@users.noreply.github.com>
2024-07-21 17:01:37 +0530

    Update settings.py

--- TravellerWebsite/settings.py
+++ TravellerWebsite/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = '/vercel/path0/staticfiles'\
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
