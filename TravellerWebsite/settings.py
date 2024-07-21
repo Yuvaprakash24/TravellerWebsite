777edf565aa3329147d0df0fe00b9bddc97b6f32
Yuvaprakash24 <114161768+Yuvaprakash24@users.noreply.github.com>
2024-07-21 09:19:49 +0530

    Update settings.py

--- TravellerWebsite/settings.py
+++ TravellerWebsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%xvk#i-*1=$%g_vibjox90*-h8)vy73-l(juur%5z%d1(dg!ce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
