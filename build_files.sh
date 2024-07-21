1655278fe7c95c7c28d1aa04b7c35d4cea612c5d
Yuvaprakash24 <114161768+Yuvaprakash24@users.noreply.github.com>
2024-07-21 20:08:59 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,8 @@
 pip3 install -r requirements.txt
 python3 manage.py collectstatic
 mkdir -p staticfiles_build/media
-cp -r media/* staticfiles_build/media/
+
+# Check if the media directory exists and copy its contents if it does
+if [ -d "media" ]; then
+  cp -r media/* staticfiles_build/media/
+fi
